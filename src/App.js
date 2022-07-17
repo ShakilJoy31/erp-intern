184ff2b30c2855f51e7c9defb96f5fbe7d37bdd0
gya7329 <gya7329@gmail.com>
2022-07-17 10:41:58 +0530

    route fix

--- src/App.js
+++ src/App.js
@@ -1,51 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Desktop150 from './Components/Desktop150';
-import Inventory from './Components/Inventory/GRN/Inventory/Inventory';
-import ExecutiveDashboard from './Components/Inventory/GRN/ExecutiveDashboard/ExecutiveDashboard';
-import Purchase from './Components/Inventory/GRN/Purchase/Purchase';
-import Billing from './Components/Inventory/GRN/Billing/Billing';
-import HumanResources from './Components/Inventory/GRN/HumanResources/HumanResources';
-import StakeholderMGT from './Components/Inventory/GRN/StakeholderMGT/StakeholderMGT';
-import EstimationTenderMGT from './Components/Inventory/GRN/EstimationTenderMGT/EstimationTenderMGT';
-import ProjectMGT from './Components/Inventory/GRN/ProjectMGT/ProjectMGT';
-import Planning from './Components/Inventory/GRN/Planning/Planning';
-import FinanceAccounts from './Components/Inventory/GRN/FinanceAccounts/FinanceAccounts';
-import { useState } from 'react';
-import Login from './Components/Authentication/Login.js/Login';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Desktop150 from "./Components/Desktop150";
+import Inventory from "./Components/Inventory/GRN/Inventory/Inventory";
+import ExecutiveDashboard from "./Components/Inventory/GRN/ExecutiveDashboard/ExecutiveDashboard";
+import Purchase from "./Components/Inventory/GRN/Purchase/Purchase";
+import Billing from "./Components/Inventory/GRN/Billing/Billing";
+import HumanResources from "./Components/Inventory/GRN/HumanResources/HumanResources";
+import StakeholderMGT from "./Components/Inventory/GRN/StakeholderMGT/StakeholderMGT";
+import EstimationTenderMGT from "./Components/Inventory/GRN/EstimationTenderMGT/EstimationTenderMGT";
+import ProjectMGT from "./Components/Inventory/GRN/ProjectMGT/ProjectMGT";
+import Planning from "./Components/Inventory/GRN/Planning/Planning";
+import FinanceAccounts from "./Components/Inventory/GRN/FinanceAccounts/FinanceAccounts";
+import { useState } from "react";
+import Login from "./Components/Authentication/Login.js/Login";
 
 function App() {
-  const [grn, setGrn] = useState(''); 
-    const [gin, setGin] = useState(''); 
-    const [transferNote, setTransferNote] = useState(''); 
-    const [stockStatement, setStockStatement] = useState(''); 
-    const [AgewiseANofStock, setAgewiseANofStock] = useState(''); 
+  const [grn, setGrn] = useState("");
+  const [gin, setGin] = useState("");
+  const [transferNote, setTransferNote] = useState("");
+  const [stockStatement, setStockStatement] = useState("");
+  const [AgewiseANofStock, setAgewiseANofStock] = useState("");
   return (
     <div>
       <Navbar></Navbar>
       <Routes>
+        <Route path="/" element={<Desktop150 />} />
         <Route>
-          <Route path='/dashboard' element={<Desktop150 setGin={setGin} setGrn={setGrn} setTransferNote={setTransferNote} setStockStatement={setStockStatement} setAgewiseANofStock={setAgewiseANofStock}></Desktop150>}>
-          <Route index element={<Inventory setGin={[gin, setGin]} setGrn={[grn, setGrn]} setTransferNote={[transferNote, setTransferNote]} setStockStatement={[stockStatement, setStockStatement]} setAgewiseANofStock={[AgewiseANofStock, setAgewiseANofStock]}></Inventory>}></Route>
-          <Route path='executiveDashboard' element={<ExecutiveDashboard></ExecutiveDashboard>}></Route>
-          <Route path='purchase' element={<Purchase></Purchase>}></Route>
-          <Route path='billing' element={<Billing></Billing>}></Route>
-          <Route path='humanResources' element={<HumanResources></HumanResources>}></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <Desktop150
+                setGin={setGin}
+                setGrn={setGrn}
+                setTransferNote={setTransferNote}
+                setStockStatement={setStockStatement}
+                setAgewiseANofStock={setAgewiseANofStock}
+              ></Desktop150>
+            }
+          >
+            <Route
+              index
+              element={
+                <Inventory
+                  setGin={[gin, setGin]}
+                  setGrn={[grn, setGrn]}
+                  setTransferNote={[transferNote, setTransferNote]}
+                  setStockStatement={[stockStatement, setStockStatement]}
+                  setAgewiseANofStock={[AgewiseANofStock, setAgewiseANofStock]}
+                ></Inventory>
+              }
+            ></Route>
+            <Route
+              path="executiveDashboard"
+              element={<ExecutiveDashboard></ExecutiveDashboard>}
+            ></Route>
+            <Route path="purchase" element={<Purchase></Purchase>}></Route>
+            <Route path="billing" element={<Billing></Billing>}></Route>
+            <Route
+              path="humanResources"
+              element={<HumanResources></HumanResources>}
+            ></Route>
 
-          <Route path='stakeholderMGT' element={<StakeholderMGT></StakeholderMGT>}></Route>
+            <Route
+              path="stakeholderMGT"
+              element={<StakeholderMGT></StakeholderMGT>}
+            ></Route>
 
-          <Route path='estimationTenderMGT' element={<EstimationTenderMGT></EstimationTenderMGT>}></Route>
+            <Route
+              path="estimationTenderMGT"
+              element={<EstimationTenderMGT></EstimationTenderMGT>}
+            ></Route>
 
-          <Route path='projectMGT' element={<ProjectMGT></ProjectMGT>}></Route>
+            <Route
+              path="projectMGT"
+              element={<ProjectMGT></ProjectMGT>}
+            ></Route>
 
-          <Route path='planning' element={<Planning></Planning>}></Route>
+            <Route path="planning" element={<Planning></Planning>}></Route>
 
-          <Route path='financeAccount' element={<FinanceAccounts></FinanceAccounts>}></Route>
+            <Route
+              path="financeAccount"
+              element={<FinanceAccounts></FinanceAccounts>}
+            ></Route>
           </Route>
         </Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Authentication/Login.js/Login.js
+++ src/Components/Authentication/Login.js/Login.js
@@ -1,25 +1,66 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div style={{backgroundColor: '#E5E5E5'}} className='flex justify-center w-full min-h-screen'>
-            <div class="">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 style={{color: '#366E83', fontFamily: 'Nunito', fontFamily: 'Nunito', fontSize: '36px', width: '257px', height: '49px'}} class='font-bold'>Welcome Back</h1>
-                        <p style={{color: '#366E83', fontFamily: 'Nunito', fontSize: '24px', width: '360px', height: '33px'}} class="">We are happy to have you back!</p>
+  return (
+    <div
+      style={{ backgroundColor: "#E5E5E5" }}
+      className="flex justify-center w-full min-h-screen"
+    >
+      <div class="">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://placeimg.com/260/400/arch"
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1
+              style={{
+                color: "#366E83",
+                fontFamily: "Nunito",
+                fontFamily: "Nunito",
+                fontSize: "36px",
+                width: "257px",
+                height: "49px",
+              }}
+              class="font-bold"
+            >
+              Welcome Back
+            </h1>
+            <p
+              style={{
+                color: "#366E83",
+                fontFamily: "Nunito",
+                fontSize: "24px",
+                width: "360px",
+                height: "33px",
+              }}
+              class=""
+            >
+              We are happy to have you back!
+            </p>
 
-                        <div>
-                        <input type="text" placeholder="Email or Phone Number" class="input w-full max-w-lg my-[25px]" />
+            <div>
+              <input
+                type="text"
+                placeholder="Email or Phone Number"
+                class="input w-full max-w-lg my-[25px]"
+              />
 
-                        <input type="text" placeholder="Password" class="input w-full max-w-lg" />
-                        </div>
-                    </div>
-                </div>
+              <input
+                type="text"
+                placeholder="Password"
+                class="input w-full max-w-lg"
+              />
             </div>
+            <button>
+              <Link to="/dashboard">Submit</Link>
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
