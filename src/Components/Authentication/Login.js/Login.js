570b24f96041412ef140f10f72aa883da3634bd4
ShakilJoy31 <joy31a@gmail.com>
2022-07-20 15:13:59 +0600

    log in route is done

--- src/Components/Authentication/Login.js/Login.js
+++ src/Components/Authentication/Login.js/Login.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import meetingLogin from './../../icons/pana.svg';
+import './Login.css'; 
 
 const Login = () => {
   return (
     <div
       style={{ backgroundColor: "#E5E5E5" }}
-      className="flex justify-center w-full min-h-screen"
+      className="flex justify-center w-full min-h-screen background-login-image"
     >
-      <div class="">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://placeimg.com/260/400/arch"
-            class="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
+      <div>
+        <div class="hero-content">
+          <div className='input-field-get-position'>
             <h1
               style={{
                 color: "#366E83",
@@ -44,18 +42,30 @@ const Login = () => {
               <input
                 type="text"
                 placeholder="Email or Phone Number"
-                class="input w-full max-w-lg my-[25px]"
+                class="input w-full max-w-lg my-[25px] login-input"
               />
 
+              <div class="form-control w-full max-w-">
               <input
                 type="text"
                 placeholder="Password"
-                class="input w-full max-w-lg"
+                class="input w-full max-w-lg login-input"
               />
+                <label class="label flex justify-end">
+                  <span class="label-text-alt forget-password forHover mb-[20px] mt-[12px]">Forget Password</span>
+                </label>
+              </div>
             </div>
-            <button>
-              <Link to="/dashboard">Submit</Link>
-            </button>
+            <div>
+            <button class="btn w-full mt-2 login-button">Login</button>
+            </div>
+          </div>
+
+          <div className='mb-[331px] mt-[159px] ml-[170px]'>
+          <img
+            src={meetingLogin}
+            class="max-w-sm rounded-lg shadow-2xl login-picture"
+            alt='' />
           </div>
         </div>
       </div>
